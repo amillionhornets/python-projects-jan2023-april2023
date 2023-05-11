0f4b7a4d4c589b276a434b70c85ae47093c994f6
amillionhornets <dylan.j.clark319@gmail.com>
2023-05-11 15:15:34 -0500

    blackjack update

--- Python/pythonProjects/blackjackProject/app.py
+++ Python/pythonProjects/blackjackProject/app.py
@@ -1,8 +1,24 @@
+from random import choices, randint
+import csv
+
+def bust():
+    pass
+
+def hit():
+    pass
+
+def dealCards():
+    with open(r"Python\pythonProjects\blackjackProject\cards.csv", "r") as cardFile:
+        csvReader = csv.reader(cardFile, delimiter=",")
+        next(csvReader)
+        for i in range(2):
+            randRow = randint(2,53)
+            
 
 
 
 def main():
-    pass
+    dealCards()
 
 if __name__ == "__main__":
     main()

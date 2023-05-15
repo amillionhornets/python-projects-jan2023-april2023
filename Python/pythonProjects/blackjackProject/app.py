f279be57691870fa53fe0c75e5b866bea0d09c6b
amillionhornets <dylan.j.clark319@gmail.com>
2023-05-15 15:09:57 -0500

    Python blackjack deal cards created

--- Python/pythonProjects/blackjackProject/app.py
+++ Python/pythonProjects/blackjackProject/app.py
@@ -1,24 +1,57 @@
 from random import choices, randint
 import csv
+from re import sub,split
 
 def bust():
     pass
 
-def hit():
-    pass
+def hit(playerHand, usedCards):
+    draw = ""
+    with open(r"Python\pythonProjects\blackjackProject\cards.csv") as csvFile:
+        pass
+    while draw not in usedCards:
+        pass
 
-def dealCards():
+def dealCards(usedCards):
     with open(r"Python\pythonProjects\blackjackProject\cards.csv", "r") as cardFile:
         csvReader = csv.reader(cardFile, delimiter=",")
         next(csvReader)
-        for i in range(2):
+        playerHand = []
+        dealerHand = []
+        card = ""
+        for i in range(4):
             randRow = randint(2,53)
-            
-
-
-
+            for index, row in enumerate(cardFile):
+                if index == randRow:
+                    card = sub("\n","",row)
+            if (i % 2) == 0:
+                playerHand.append(card)
+            else:
+                dealerHand.append(card)
+        usedCards = dealerHand + playerHand + usedCards
+        return {"Player Hand": playerHand, "Dealer Hand": dealerHand, "Used Cards": usedCards}
 def main():
-    dealCards()
+    usedCards = []
+    playerCard1 = split(",", dealCards(usedCards)["Player Hand"][0]) 
+    playerCard2 = split(",", dealCards(usedCards)["Player Hand"][1])
+    dealerHand = dealCards(usedCards)["Dealer Hand"]
+    usedCards = dealCards(usedCards)["Used Cards"]
+    playerTotal = 0
+    print(playerCard1[0])
+    print(playerCard2[0])
+    if playerCard1[0] == '"ace"':
+        playerTotal = str(11 + int(playerCard2[2])) + " or " + str(1 + int(playerCard2[2]))
+    elif playerCard2[0] == '"ace"':
+        playerTotal = str(11 + int(playerCard1[2])) + " or " + str(1 + int(playerCard1[2]))
+    else:
+        playerTotal = int(playerCard1[2]) + int(playerCard2[2])
+    print("Your total is " + str(playerTotal))
+    print("Would like to Hit(h) or Stand(s): ")
+    choice = input()
+    if choice == "h":
+        hit()
+    else:
+        pass
 
 if __name__ == "__main__":
     main()
